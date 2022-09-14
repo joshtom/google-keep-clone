c6c1052558ef699d9931813b29a04a550ddf4997
Joshua <olajidejoshua4real@gmail.com>
2022-09-14 19:37:16 +0100

    Initial folder setup

---
+++ src/components/ExampleComponent/index.jsx
@@ -0,0 +1,13 @@
+/**
+ * This is an Example component and all component should follow this structure
+ * @param { props } -> Props can be destructured like this
+ * @package { prop-types }: Usage of prop types is also allowed to validate all input props
+ */
+
+const ExampleComponent = () => {
+    return (
+        <h1> Example Component </h1>
+    )
+}
+
+export default ExampleComponent;
