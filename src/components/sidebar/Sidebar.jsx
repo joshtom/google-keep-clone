844d42bf4dd8c82d508f8076d41c551e00fc9ffa
Greattie <gamubode@gmail.com>
2022-10-10 16:13:53 +0100

    Updated sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from './styles/sidebar.module.css'
+import styles from './styles/sidebar.module.scss'
 import { SidebarData } from "./SidebarData";
 
 const Sidebar = () => {
